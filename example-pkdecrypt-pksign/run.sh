b75c9f500db3133ae5962938328cd217cf929c55
Jari O. O. Pennanen <ciantic@oksidi.com>
2022-11-11 17:06:09 +0200

    ...

--- example-pkdecrypt-pksign/run.sh
+++ example-pkdecrypt-pksign/run.sh
@@ -7,11 +7,12 @@
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 
 if [ $sourced -eq 0 ]; then
+    echo "⤴ Stop on fail"
     set -e
 fi
 
 
-GNUPGHOME="/tmp/tmp.gpgtests.gpgagent/"
+GNUPGHOME="/tmp/tmp.gpgtests.gpgagent"
 GPG_TTY=$(tty)
 export GNUPGHOME
 export GPG_TTY
@@ -20,19 +21,18 @@ rm -r $GNUPGHOME || true
 mkdir -p $GNUPGHOME
 chmod 700 $GNUPGHOME
 
-killall gpg-agent || true
 
 gpg --quiet --batch --passphrase '' --default-new-key-algo "ed25519/cert,auth,sign+cv25519/encr" --quick-generate-key "John Doe <john@example.com>"
+
+# It's important to close gpg agents after `gpg` command, otherwise some of the
+# instances might cause race conditions for the tests below
+killall gpg-agent || true
 
 # Test encrypt and decrypt
 # echo "swordfish" | gpg --quiet --trust-model always --recipient John --armor --sign --encrypt | gpg --quiet --trust-model always --decrypt
 
 KEYGRIPS=$(gpg --list-keys --with-keygrip | grep -Po '(?<=Keygrip = )(.*)')
-
-# Get first line
 PRIMARY_KEYGRIP=$(echo "$KEYGRIPS" | head -n 1)
-
-# Get second line
 ENCRYPT_KEYGRIP=$(echo "$KEYGRIPS" | tail -n 1)
 
 echo "Primary keygrip: $PRIMARY_KEYGRIP"
@@ -49,31 +49,28 @@ allow-mark-trusted
 log-file gpg-agent.log
 " > $GNUPGHOME/gpg-agent.conf
 
-
-# Do encryption on ephemeral and s values
-echo ""
-echo "⌛ Try PKDECRYPT ..."
-
-echo "(7:enc-val (4:ecdh (1:e:12341234)(1:s:12341234)))" > $GNUPGHOME/ciphertext.txt
-
+# Decrypting example
 echo "
-/definqfile CIPHERTEXT $GNUPGHOME/ciphertext.txt
+/hex
+/let MYCIPHER (7:enc-val (4:ecdh (1:e:12341234)(1:s:12341234)))
+/definq CIPHERTEXT MYCIPHER
+# Or using file
+# /definqfile CIPHERTEXT ciphertext.txt
 SETKEY $ENCRYPT_KEYGRIP
 PKDECRYPT
-" > $GNUPGHOME/decrypt_example.txt
+" > $GNUPGHOME/example_decrypt.txt
 
-gpg-connect-agent --run $GNUPGHOME/decrypt_example.txt --homedir $GNUPGHOME --verbose /bye
-
-echo "✅ PKDECRYPT, If you see above  D (5:value33:...)"
-echo ""
-echo "⌛ Try PKSIGN ..."
-
+# Signing example
 echo "
+/hex
 SIGKEY $PRIMARY_KEYGRIP
 SETHASH --hash=sha512 12345678901234567890123456789014
 PKSIGN
-" > $GNUPGHOME/sign_example.txt
+" > $GNUPGHOME/example_signing.txt
 
-gpg-connect-agent --run $GNUPGHOME/sign_example.txt --homedir $GNUPGHOME --verbose /bye
+# Run examples
+gpg-connect-agent --run $GNUPGHOME/example_decrypt.txt --homedir $GNUPGHOME --verbose /bye
+gpg-connect-agent --run $GNUPGHOME/example_signing.txt --homedir $GNUPGHOME --verbose /bye
 
-echo "✅ PKSIGN, If you see above  D (7:sig-val(5:eddsa(1:r32:...)(1:s32:...)))"
+echo "✅ PKDECRYPT, If you see above hex (5:value33:...)"
+echo "✅ PKSIGN, If you see above hex (7:sig-val(5:eddsa(1:r32:...)(1:s32:...)))"
